efea8a3c40ebf5b03578f7e9c26be27525b8e638
kevinjel <kevinjel@devxps.lan>
2020-05-28 23:07:52 +0200

    Set cron, restart dockerbox, wip: cryptomator role

--- scripts/2-prepare_ansible.sh
+++ scripts/2-prepare_ansible.sh
@@ -5,6 +5,8 @@ install_venv() {
    sudo apt-get install python3-venv -y
    sudo apt-get install aptitude -y
    sudo apt-get install python-apt-common -y
+   sudo apt-get install pylint -y
+   sudo apt-get install python3-autopep8 -y
 }
 
 make_venv_dir() {
